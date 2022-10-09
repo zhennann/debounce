906fb86bc3c4576f7c2ac27e6e085158a8213336
zhennann <zhen.nann@icloud.com>
2022-10-09 15:15:14 +0800

    feat: debounce

--- dist/index.js
+++ dist/index.js
@@ -36,80 +36,22 @@
 var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */   "default": () => (/* binding */ debounce)
 /* harmony export */ });
-var PREFIX_A = '/api/';
-var PREFIX_B = 'egg-born-module-';
-var PREFIX_C = './egg-born-module-';
-var PREFIX_D = './';
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
-  parseInfo: function parseInfo(moduleName) {
-    var type = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'module';
-    if (!moduleName) return null;
-    if (moduleName.indexOf('://') > -1) return null;
-    if (moduleName.charAt(0) === '/') moduleName = moduleName.substr(1);
-    var parts = moduleName.split('/').filter(function (item) {
-      return item;
-    });
-
-    if (parts.length < 2) {
-      parts = moduleName.split('-').filter(function (item) {
-        return item;
-      });
-      if (parts.length < 2) return null;
-      if (parts.length >= 5) parts = parts.slice(3);
+function debounce(func, wait) {
+  var i = 0;
+  return function () {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
     }
 
-    if (type === 'suite') {
-      return {
-        pid: parts[0],
-        name: parts[1],
-        fullName: "egg-born-suite-".concat(parts[0], "-").concat(parts[1]),
-        relativeName: "".concat(parts[0], "-").concat(parts[1])
-      };
-    }
-
-    return {
-      pid: parts[0],
-      name: parts[1],
-      fullName: "egg-born-module-".concat(parts[0], "-").concat(parts[1]),
-      relativeName: "".concat(parts[0], "-").concat(parts[1]),
-      url: "".concat(parts[0], "/").concat(parts[1]),
-      sync: parts[2] === 'sync',
-      monkey: parts[2] === 'monkey'
-    };
-  },
-  parseName: function parseName(moduleUrl) {
-    if (!moduleUrl) return null;
-
-    if (moduleUrl.indexOf('/api/static/') === 0) {
-      moduleUrl = '/api/' + moduleUrl.substring('/api/static/'.length);
-    }
-
-    if (moduleUrl.indexOf(PREFIX_A) === 0) {
-      var posA = PREFIX_A.length;
-      var posB = moduleUrl.indexOf('/', posA) + 1;
-      if (posB === -1) return null;
-      var posC = moduleUrl.indexOf('/', posB);
-      if (posC === -1) return null;
-      return moduleUrl.substring(posA, posC);
-    } else if (moduleUrl.indexOf(PREFIX_B) === 0) {
-      return this._parseName(moduleUrl, PREFIX_B);
-    } else if (moduleUrl.indexOf(PREFIX_C) === 0) {
-      return this._parseName(moduleUrl, PREFIX_C);
-    } else if (moduleUrl.indexOf(PREFIX_D) === 0) {
-      return this._parseName(moduleUrl, PREFIX_D);
-    }
-
-    return null;
-  },
-  _parseName: function _parseName(moduleUrl, prefix) {
-    var posA = prefix.length;
-    var posB = moduleUrl.indexOf('/', posA);
-    if (posB === -1) posB = moduleUrl.length;
-    return moduleUrl.substring(posA, posB);
-  }
-});
+    var ctx = this;
+    if (i !== 0) clearTimeout(i);
+    i = setTimeout(function () {
+      func.apply(ctx, args);
+    }, wait);
+  };
+}
 module.exports = __webpack_exports__;
 /******/ })()
 ;
